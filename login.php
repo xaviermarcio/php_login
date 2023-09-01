27cdc69516528acc3c155b55ba095e7b29cc8f3c
Márcio Xavier <43189549+xaviermarcio@users.noreply.github.com>
2023-09-01 13:27:39 -0300

    Ajustando os arquivos php

--- login.php
+++ login.php
@@ -4,9 +4,9 @@ include('conexao.php');
 if(isset($_POST['email']) || isset($_POST['senha'])) {
 
     if(strlen($_POST['email']) == 0) {
-        echo "Preencha seu e-mail";
+       echo "<script>alert('Digite seu email!');</script>";
     } else if(strlen($_POST['senha']) == 0) {
-        echo "Preencha sua senha";
+       echo "<script>alert('Digite sua senha!');</script>"; 
     } else {
 
         $email = $mysqli->real_escape_string($_POST['email']);
@@ -31,7 +31,7 @@ if(isset($_POST['email']) || isset($_POST['senha'])) {
             header("Location: painel.php");
 
         } else {
-            echo "Falha ao logar! E-mail ou senha incorretos";
+            echo "<script>alert('Falha ao logar! E-mail ou senha incorretos')</script>";
         }
 
     }
--- painel.php
+++ painel.php
@@ -1,6 +1,6 @@
 <?php
 
-include('protect.php');
+include('./protect.php');
 
 ?>
 <!DOCTYPE html>
@@ -15,7 +15,7 @@ include('protect.php');
     Bem vindo ao Painel, <?php echo $_SESSION['nome']; ?>.
 
     <p>
-        <a href="logout.php">Sair</a>
+        <a href="./logout.php">Sair</a>
     </p>
 </body>
 </html>
